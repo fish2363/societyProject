9eaa599785bad8cbfd1c2882bd98366c3822bf29
sobinkim <sobinkim95@gmail.com>
2025-08-11 10:16:36 -0400

    Grid_Build improved-ver

--- Assets/03.Member/SB/Code/GridBuildingSystem.cs
+++ Assets/03.Member/SB/Code/GridBuildingSystem.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using Unity.VisualScripting;
+using UnityEditor.PackageManager;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 public class GridBuildingSystem : MonoBehaviour
@@ -7,11 +9,15 @@ public class GridBuildingSystem : MonoBehaviour
     public GameObject BlockToBuild;
     private GameObject PreviewBlock;
     public LayerMask CanCreateLayerMask;
+    private Vector3 previewBlockPos;
 
     private Vector3 lastSelectedPosition;
+    private Vector3 detectScale;
     private void SetPriviewBlock(GameObject gameObject)
     {
-      
+        if(PreviewBlock)
+            Destroy(PreviewBlock);
+
         PreviewBlock = Instantiate(gameObject, Vector3.zero, Quaternion.identity);
         PreviewBlock.GetComponent<BoxCollider>().isTrigger = false;
 
@@ -24,6 +30,11 @@ public class GridBuildingSystem : MonoBehaviour
         }
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(previewBlockPos, detectScale);
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
@@ -38,34 +49,41 @@ public class GridBuildingSystem : MonoBehaviour
         if (PreviewBlock)
         {
             Vector3 CellPos = grid.GetCellCenterWorld(cell);
-            Vector3 previewBlockPos = new Vector3(CellPos.x, CellPos.y + (PreviewBlock.transform.localScale.y / 2 - 0.6f), CellPos.z);
+            previewBlockPos = new Vector3(CellPos.x, CellPos.y + (PreviewBlock.transform.localScale.y / 2 - 0.6f), CellPos.z);
 
             PreviewBlock.transform.position = previewBlockPos;
 
             if (Input.GetMouseButtonDown(0))
             {
-                Vector3 rayStart = PreviewBlock.transform.position + Vector3.up *10f;
-
-                if (Physics.Raycast(rayStart, Vector3.down, out RaycastHit hitInfo, 100f))
+                
+                detectScale = PreviewBlock.transform.localScale;
+                detectScale = new Vector3(detectScale.x - 0.4f, detectScale.y - 0.4f,detectScale.z - 0.4f);
+                Collider[] colliders = Physics.OverlapBox(previewBlockPos, detectScale/2);
+             
+                bool canCreate = true;
+                foreach (Collider collider in colliders)
                 {
-                    
-                    if (hitInfo.collider.gameObject.layer != LayerMask.NameToLayer("CantCreate"))
+                    if(collider.gameObject.layer == LayerMask.NameToLayer("CantCreate"))
                     {
-                      
-                        GameObject go = Instantiate(BlockToBuild, previewBlockPos, Quaternion.identity);
-                        go.layer = LayerMask.NameToLayer("CantCreate");
-                    }
-                    else
-                    {
-                        Debug.Log("이미 설치된 블록이 있습니다.");
+                        canCreate = false;
                     }
                 }
 
+                if (canCreate)
+                {
+
+                    GameObject go = Instantiate(BlockToBuild, previewBlockPos, Quaternion.identity);
+                    go.layer = LayerMask.NameToLayer("CantCreate");
+                }
+                else
+                {
+                    Debug.Log("이미 설치된 블록이 있습니다.");
+                }
                 Destroy(PreviewBlock);
             }
         }
     }
-
+    
     RaycastHit TryGetRaycastHit(Vector3 mousePos)
     {
         mousePos.z = Camera.main.nearClipPlane;
