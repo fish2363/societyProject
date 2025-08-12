5911ee261065e5d2e1a64f43b333981447c5e82a
sobinkim <sobinkim95@gmail.com>
2025-08-12 14:01:14 +0900

    last

--- Assets/03.Member/SB/Code/GridBuildingSystem.cs
+++ Assets/03.Member/SB/Code/GridBuildingSystem.cs
@@ -5,23 +5,25 @@ using UnityEngine.UIElements;
 
 public class GridBuildingSystem : MonoBehaviour
 {
-    public Grid grid;
-    public GameObject BlockToBuild;
-    private GameObject PreviewBlock;
-    public LayerMask CanCreateLayerMask;
-    private Vector3 previewBlockPos;
+    [SerializeField] private Grid grid;
+    public GameObject blockToBuild;
+    private GameObject previewBlock;
+    [SerializeField] private LayerMask CanCreateLayerMask;
 
+    private Vector3 previewBlockPos;
     private Vector3 lastSelectedPosition;
     private Vector3 detectScale;
-    private void SetPriviewBlock(GameObject gameObject)
+
+    
+    public void SetPriviewBlock(GameObject gameObject)
     {
-        if(PreviewBlock)
-            Destroy(PreviewBlock);
+        if(previewBlock)
+            Destroy(previewBlock);
 
-        PreviewBlock = Instantiate(gameObject, Vector3.zero, Quaternion.identity);
-        PreviewBlock.GetComponent<BoxCollider>().isTrigger = false;
+        previewBlock = Instantiate(gameObject, Vector3.zero, Quaternion.identity);
+        previewBlock.GetComponent<BoxCollider>().isTrigger = false;
 
-        foreach (Renderer block in PreviewBlock.GetComponentsInChildren<Renderer>())
+        foreach (Renderer block in previewBlock.GetComponentsInChildren<Renderer>())
         {
             Material m = block.material;
             Color c = Color.green;
@@ -39,24 +41,24 @@ public class GridBuildingSystem : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            SetPriviewBlock(BlockToBuild);
+            SetPriviewBlock(blockToBuild);
         }
 
         RaycastHit hit = TryGetRaycastHit(Input.mousePosition);
         Vector3 selectedPos = hit.point;
         Vector3Int cell = grid.WorldToCell(selectedPos);
 
-        if (PreviewBlock)
+        if (previewBlock)
         {
             Vector3 CellPos = grid.GetCellCenterWorld(cell);
-            previewBlockPos = new Vector3(CellPos.x, CellPos.y + (PreviewBlock.transform.localScale.y / 2 - 0.6f), CellPos.z);
+            previewBlockPos = new Vector3(CellPos.x, CellPos.y + (previewBlock.transform.localScale.y / 2 - 0.6f), CellPos.z);
 
-            PreviewBlock.transform.position = previewBlockPos;
+            previewBlock.transform.position = previewBlockPos;
 
             if (Input.GetMouseButtonDown(0))
             {
                 
-                detectScale = PreviewBlock.transform.localScale;
+                detectScale = previewBlock.transform.localScale;
                 detectScale = new Vector3(detectScale.x - 0.4f, detectScale.y - 0.4f,detectScale.z - 0.4f);
                 Collider[] colliders = Physics.OverlapBox(previewBlockPos, detectScale/2);
              
@@ -72,14 +74,14 @@ public class GridBuildingSystem : MonoBehaviour
                 if (canCreate)
                 {
 
-                    GameObject go = Instantiate(BlockToBuild, previewBlockPos, Quaternion.identity);
+                    GameObject go = Instantiate(blockToBuild, previewBlockPos, Quaternion.identity);
                     go.layer = LayerMask.NameToLayer("CantCreate");
                 }
                 else
                 {
                     Debug.Log("이미 설치된 블록이 있습니다.");
                 }
-                Destroy(PreviewBlock);
+                Destroy(previewBlock);
             }
         }
     }
