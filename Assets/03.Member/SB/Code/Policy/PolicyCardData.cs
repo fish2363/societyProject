c42bfc725abc552fee39d2b534988fc89db8569a
sobinkim <sobinkim95@gmail.com>
2025-08-17 10:53:45 -0400

    PolicyCard

---
+++ Assets/03.Member/SB/Code/Policy/PolicyCardData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+[CreateAssetMenu(fileName = "PollicyCard", menuName = "PollicyCardData")]
+public class PolicyCardData : ScriptableObject
+{
+    public string policyName;
+    public float policyCost;
+    public PolicyType policyType;
+    public PolicyEffectBase policyEffect;
+    public Sprite policyTypeIcon;
+    public Sprite Icon;
+
+    public string description;
+}
+
+public enum PolicyType
+{
+    Ecofriendly,
+    Industryfriendly
+}
